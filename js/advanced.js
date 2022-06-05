df0c5e920df42324c485b8aa48fb72fb19750a5a
Godnon Dsilva <godnondsilva@gmail.com>
2022-06-05 19:00:54 +0530

    Fix advanced calculator styles bug

---
+++ js/advanced.js
@@ -0,0 +1,134 @@
+// ----- Variables -----
+var canUseDot = true;
+var canUseTask = true;
+var errOccured = false;
+
+//----- functions -----
+const clearLast = () => {
+	var lastValue = document
+		.getElementById('ans')
+		.value.substring(0, document.getElementById('ans').value.length - 1);
+	document.getElementById('ans').value = lastValue;
+	if (errOccured) {
+		clearall();
+	} else if (!lastValue.includes('.')) {
+		canUseDot = true;
+	} else if (
+		!lastValue.includes('+') ||
+		!lastValue.includes('-') ||
+		!lastValue.includes('/') ||
+		!lastValue.includes('*')
+	) {
+		canUseTask = true;
+	}
+};
+
+const clearall = () => {
+	document.getElementById('ans').value = '';
+	canUseDot = true;
+	canUseTask = true;
+	errOccured = false;
+};
+
+const addNumber = (val) => {
+	if (errOccured) {
+		clearall();
+	}
+	document.getElementById('ans').value += val;
+	if (
+		document.getElementById('ans').value < 1 &&
+		document.getElementById('ans') === '.'
+	) {
+		document.getElementById('ans').value = '0';
+	}
+	canUseTask = true;
+};
+
+const addTask = (task) => {
+	if (errOccured) {
+		clearall();
+		document.getElementById('ans').value = '0' + task;
+	} else if (document.getElementById('ans').value < 1 && canUseTask) {
+		document.getElementById('ans').value = '0' + task;
+	} else if (canUseTask) {
+		document.getElementById('ans').value += task;
+	}
+	canUseDot = true;
+	canUseTask = false;
+};
+const addOpenBracket = () => {
+	document.getElementById('ans').value += '(';
+	if (errOccured) {
+		clearall();
+	}
+};
+
+const addCloseBracket = () => {
+	document.getElementById('ans').value += ')';
+	if (errOccured) {
+		clearall();
+	}
+};
+
+constpi = () => {
+	if (errOccured) {
+		clearall();
+	}
+	return (document.getElementById('ans').value += Math.PI);
+};
+
+const sqrtOf = () => {
+	calc();
+	var valueInDisplay = document
+		.getElementById('ans')
+		.value.substring(0, document.getElementById('ans').value.length);
+	return (document.getElementById('ans').value = Math.sqrt(valueInDisplay));
+};
+
+const cbrtOf = () => {
+	calc();
+	var valueInDisplay = document
+		.getElementById('ans')
+		.value.substring(0, document.getElementById('ans').value.length);
+	return (document.getElementById('ans').value = Math.cbrt(valueInDisplay));
+};
+
+const rndOf = () => {
+	calc();
+	var valueInDisplay = document
+		.getElementById('ans')
+		.value.substring(0, document.getElementById('ans').value.length);
+	return (document.getElementById('ans').value = Math.round(valueInDisplay));
+};
+
+const calc = () => {
+	try {
+		if (document.getElementById('ans').value === 'Error') {
+			throw err;
+		} else if (document.getElementById('ans').value) {
+			document.getElementById('ans').value = eval(
+				document.getElementById('ans').value,
+			);
+		} else if (!document.getElementById('ans').value) {
+			return;
+		} else if (document.getElementById('ans').value.includes('.')) {
+			canUseDot = false;
+		}
+	} catch (err) {
+		console.log('WARNING!');
+		document.getElementById('ans').value = 'Error';
+		errOccured = true;
+	}
+};
+
+const dot = () => {
+	if (errOccured) {
+		clearall();
+		document.getElementById('ans').value += '.';
+	} else if (canUseDot) {
+		document.getElementById('ans').value += '.';
+		canUseDot = false;
+	} else {
+		console.log('WARNING: DOT ALREADY PRESENT!');
+	}
+};
--- js/simple.js
+++ js/simple.js
@@ -5,93 +5,102 @@ var errOccured = false;
 
 //----- functions -----
 const clearLast = () => {
-  var lastValue = document.getElementById("ans").value.substring(0,document.getElementById("ans").value.length-1);
-  document.getElementById("ans").value = lastValue;
-  if(errOccured) {
-      clearall();
-  } else if (!lastValue.includes('.')) {
-    canUseDot = true;
-  } else if(!lastValue.includes('+') || !lastValue.includes('-') ||
-     !lastValue.includes('/') || !lastValue.includes('*')) {
-      canUseTask = true;
-  }
-}
+	var lastValue = document
+		.getElementById('ans')
+		.value.substring(0, document.getElementById('ans').value.length - 1);
+	document.getElementById('ans').value = lastValue;
+	if (errOccured) {
+		clearall();
+	} else if (!lastValue.includes('.')) {
+		canUseDot = true;
+	} else if (
+		!lastValue.includes('+') ||
+		!lastValue.includes('-') ||
+		!lastValue.includes('/') ||
+		!lastValue.includes('*')
+	) {
+		canUseTask = true;
+	}
+};
 
 const clearall = () => {
-  document.getElementById("ans").value="";
-  canUseDot = true;
-  canUseTask = true;
-  errOccured = false;
-}
+	document.getElementById('ans').value = '';
+	canUseDot = true;
+	canUseTask = true;
+	errOccured = false;
+};
 
 const addNumber = (val) => {
-  if (errOccured) {
-    clearall();
-  }
-  document.getElementById("ans").value += val;
-  if((document.getElementById("ans").value < 1) && (document.getElementById("ans") === ".")) {
-    document.getElementById("ans").value = "0";
-  }
-  canUseTask = true;
-}
+	if (errOccured) {
+		clearall();
+	}
+	document.getElementById('ans').value += val;
+	if (
+		document.getElementById('ans').value < 1 &&
+		document.getElementById('ans') === '.'
+	) {
+		document.getElementById('ans').value = '0';
+	}
+	canUseTask = true;
+};
 
 const addTask = (task) => {
-  if(canUseTask) {
-      document.getElementById("ans").value += task;
-  } else if(document.getElementById("ans").value < 1 && canUseTask){
-      document.getElementById("ans").value = "0" + task;
-  } else if(errOccured) {
-      clearall();
-      document.getElementById("ans").value = "0" + task;
-  }
-  canUseDot = true;
-  canUseTask = false;
-}
+	if (canUseTask) {
+		document.getElementById('ans').value += task;
+	} else if (document.getElementById('ans').value < 1 && canUseTask) {
+		document.getElementById('ans').value = '0' + task;
+	} else if (errOccured) {
+		clearall();
+		document.getElementById('ans').value = '0' + task;
+	}
+	canUseDot = true;
+	canUseTask = false;
+};
 
 const addOpenBracket = () => {
-  document.getElementById("ans").value += "(";
-  if(errOccured) {
-      clearall();
-  }
-}
+	document.getElementById('ans').value += '(';
+	if (errOccured) {
+		clearall();
+	}
+};
 
 const addCloseBracket = () => {
-  document.getElementById("ans").value += ")";
-  if(errOccured) {
-      clearall();
-  }
-}
+	document.getElementById('ans').value += ')';
+	if (errOccured) {
+		clearall();
+	}
+};
 
 const calc = () => {
-  try {
-    if(document.getElementById("ans").value === "Error") {
-      throw err;
-    } else if(document.getElementById("ans").value) {
-      document.getElementById("ans").value = eval(document.getElementById("ans").value);
-    } else if(!document.getElementById("ans").value){
-      return;
-    } else if(document.getElementById("ans").value.includes(".")) {
-      canUseDot = false;
-    }
-  }
-  catch(err) {
-    console.log('WARNING!');
-    document.getElementById("ans").value = "Error";
-    errOccured = true;
-  }
-}
+	try {
+		if (document.getElementById('ans').value === 'Error') {
+			throw err;
+		} else if (document.getElementById('ans').value) {
+			document.getElementById('ans').value = eval(
+				document.getElementById('ans').value,
+			);
+		} else if (!document.getElementById('ans').value) {
+			return;
+		} else if (document.getElementById('ans').value.includes('.')) {
+			canUseDot = false;
+		}
+	} catch (err) {
+		console.log('WARNING!');
+		document.getElementById('ans').value = 'Error';
+		errOccured = true;
+	}
+};
 
 const dot = () => {
-    if(errOccured) {
-        clearall();
-        canUseDot = false;
-        document.getElementById("ans").value += ".";
-    } else if(canUseDot) {
-      document.getElementById("ans").value += ".";
-      canUseDot = false;
-
-    } else {
-      canUseDot = false;
-      console.log('WARNING: DOT ALREADY PRESENT!');
-    }
-}
+	if (errOccured) {
+		clearall();
+		canUseDot = false;
+		document.getElementById('ans').value += '.';
+	} else if (canUseDot) {
+		document.getElementById('ans').value += '.';
+		canUseDot = false;
+	} else {
+		canUseDot = false;
+		console.log('WARNING: DOT ALREADY PRESENT!');
+	}
+};
